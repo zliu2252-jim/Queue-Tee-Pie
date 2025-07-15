3597d88fca748d157ba7515bb11354315e2f962f
zliu2252-jim <zliu2252@wisc.edu>
2025-07-14 19:06:46 -0500

    Initial commit

---
+++ app/src/main/java/org/example/Cutie.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Cutie {
+  public String description(); // All cuties need to have a description of what makes them cute.
+
+  public Integer cutenessRating(); // All cuties get a cuteness rating out of ten.
+}
